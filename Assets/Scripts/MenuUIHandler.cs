3581d5a2bcb237d1a4c60815754748a971f31941
Thiago de Souza Farias <tsf@cin.ufpe.br>
2023-10-23 16:43:47 -0300

    Buttons to start and quit
    Implements buttons on the Menu Scene that allows user to start game or quit game

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
